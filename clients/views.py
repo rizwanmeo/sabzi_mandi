438fe3f5d2efa14969757f03f9d781aa67ef37d3
rizwanmeo <rizwan_meo@rocketmail.com>
2020-04-19 00:19:27 +0500

    Added filter option in client detail page

--- clients/views.py
+++ clients/views.py
@@ -60,28 +60,80 @@ class ClientDeleteView(CustomDeleteView):
         return HttpResponseRedirect(self.get_success_url())
 
 
+def get_client_detail(request, context, is_reverse=True):
+    obj = context["object"]
+
+    columns = ["id", "client__id", "client__name", "tx_id", "tx_time", "tx_date",
+               "balance", "bill_amount", "payment_amount", "description"]
+
+    qs = obj.clientledger_set.all()
+    today = datetime.date.today()
+    start_date = request.GET.get("start_date", "")
+    detail_duration = ""
+    if bool(start_date):
+        start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d").date()
+        qs = qs.filter(tx_date__gte=start_date)
+        start_date = start_date.strftime("%Y-%m-%d")
+
+    end_date = request.GET.get("end_date", "")
+    if bool(end_date):
+        end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d").date()
+        qs = qs.filter(tx_date__lte=end_date)
+        end_date = end_date.strftime("%Y-%m-%d")
+
+    if bool(start_date) and bool(end_date):
+        if start_date == end_date:
+            detail_duration = start_date
+        else:
+            detail_duration = "From %s to %s" % (start_date, end_date)
+    elif not bool(end_date):
+        detail_duration = "From %s till now" % start_date
+    elif not bool(start_date):
+        detail_duration = "From start till %s" % end_date
+    else:
+        detail_duration = "From start till now"
+
+    qs = qs.order_by("-id" if is_reverse else "id")
+    vs = list(qs.values(*columns))
+
+    data = []
+    total_payment = 0
+    opening_balance = 0
+    current_balance = 0
+    total_billed_amount = 0
+
+    for row in vs:
+        row["client"] = {"id": row.pop("client__id"), "name": row.pop("client__name")}
+        data.append(row)
+        total_payment += row["payment_amount"]
+        total_billed_amount += row["bill_amount"]
+
+    if len(data) > 0:
+        if is_reverse:
+            opening_balance = data[-1]["balance"] + data[-1]["payment_amount"] - data[-1]["bill_amount"]
+            current_balance = data[0]["balance"]
+        else:
+            opening_balance = data[0]["balance"] + data[0]["payment_amount"] - data[0]["bill_amount"]
+            current_balance = data[-1]["balance"]
+
+    context["data"] = data
+    context["start_date"] = start_date
+    context["end_date"] = end_date
+    context["total_payment"] = total_payment
+    context["total_billed_amount"] = total_billed_amount
+    context["opening_balance"] = opening_balance
+    context["current_balance"] = current_balance
+    context["detail_duration"] = detail_duration
+
+    return context
+
 class ClientDetailView(CustomDetailView):
     model = Client
     template_name = "clients/detail.html"
 
     def get_context_data(self, **kwargs):
         context = super(ClientDetailView, self).get_context_data(**kwargs)
-        obj = context["object"]
-
-        columns = ["id", "client__id", "client__name", "tx_id", "tx_time", "tx_date",
-                   "balance", "bill_amount", "payment_amount", "description"]
-
-        qs = obj.clientledger_set.all()
-        qs = qs.order_by("-id")
-        vs = list(qs.values(*columns))
-
-        data = []
-        for row in vs:
-            row["client"] = {"id": row.pop("client__id"), "name": row.pop("client__name")}
-            data.append(row)
-
-        context["data"] = data
-        return context
+        return get_client_detail(self.request, context)
 
 class ClientDetailPrintView(CustomDetailView):
     model = Client
@@ -90,17 +142,11 @@ class ClientDetailPrintView(CustomDetailView):
     def get_context_data(self, **kwargs):
         context = super(ClientDetailPrintView, self).get_context_data(**kwargs)
         obj = context["object"]
-        columns = ["client__id", "client__name", "tx_id", "tx_time", "tx_date",
-                   "balance", "bill_amount", "payment_amount", "description"]
-
-        qs = obj.clientledger_set.all()
-        qs = qs.order_by("id")
-        vs = list(qs.values(*columns))
+        context = get_client_detail(self.request, context, False)
+        vs = context["data"]
 
         data = []
         object_list = []
-        total_payment = 0
-        total_billed_amount = 0
         for count, row in enumerate(vs):
             if count == 28:
                 data.append(object_list)
@@ -108,17 +154,10 @@ class ClientDetailPrintView(CustomDetailView):
             elif count > 28 and len(object_list) == 38:
                 data.append(object_list)
                 object_list = []
-            row["client"] = {"id": row.pop("client__id"), "name": row.pop("client__name")}
             object_list.append(row)
-
-            total_payment += row["payment_amount"]
-            total_billed_amount += row["bill_amount"]
 
         if len(object_list) > 0:
             data.append(object_list)
 
         context["data"] = data
-        context["total_payment"] = total_payment
-        context["total_billed_amount"] = total_billed_amount
-
         return context
