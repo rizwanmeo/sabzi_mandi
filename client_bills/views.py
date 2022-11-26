2ad41d5b640d7c151bcefadbb1e623c1fd20ed55
Rizwan <rizwan@github.com>
2022-11-26 21:20:07 +0500

    Resolve merge conflicts

--- client_bills/views.py
+++ client_bills/views.py
@@ -10,6 +10,7 @@ from django.views.decorators.http import require_http_methods
 
 from .forms import *
 from .models import *
+from .utils import *
 from sabzi_mandi.views import *
 from clients.models import Client
 from ledger.models import ClientLedgerEditable
@@ -273,75 +274,21 @@ class BillDetailCreateView(CustomCreateView):
         request.bill = self.get_bill_obj(kwargs.get("bill_id", 0))
         return super(BillDetailCreateView, self).post(request)
 
-def done_drafted_bill(request, bill_obj):
-    billed_amount = 0
-    now = datetime.datetime.now()
-    billdetail_vs = list(bill_obj.billdetail_set.values("rate", "item_count"))
-    for detail_obj in billdetail_vs:
-        billed_amount += detail_obj["rate"] * detail_obj["item_count"]
-    bill_obj.is_draft = False
-    bill_obj.billed_amount = billed_amount
-
-    bill_obj.created_time = now
-    create_bill_ledger(bill_obj, bill_obj.client.current_balance)
-    bill_obj.client.current_balance += billed_amount
-
-    if bill_obj.payment:
-        bill_obj.payment.payment_time = now + datetime.timedelta(seconds=1)
-        bill_obj.payment.is_draft = False
-        description = "Payment received against bill ID bill-%d" % bill_obj.id
-        bill_obj.payment.description = description
-        create_payment_ledger(bill_obj.payment, bill_obj.client.current_balance)
-        bill_obj.client.current_balance -= bill_obj.payment.amount
-        bill_obj.payment.save()
-
-    bill_obj.balance = bill_obj.client.current_balance
-    bill_obj.client.save()
-    bill_obj.save()
-
-def get_print_bill_data(request, bill_obj):
-    context = {}
-    if bill_obj.payment:
-        bill_obj.previous_balance = bill_obj.balance - bill_obj.billed_amount + bill_obj.payment.amount
-    else:
-        bill_obj.previous_balance = bill_obj.balance - bill_obj.billed_amount
-    bill_obj.after_bill_balance = bill_obj.previous_balance + bill_obj.billed_amount
-
-    context["obj"] = bill_obj
-    qs = bill_obj.billdetail_set.all()
-    vs = list(qs.values("item__name", "unit", "rate", "item_count"))
-    data = []
-    total_item_count = 0
-    total_item_weight = 0
-    for row in vs:
-        detail_obj = {}
-        detail_obj["name"] = row["item__name"]
-        detail_obj["rate"] = row["rate"]
-        detail_obj["item_count"] = row["item_count"]
-        detail_obj["amount"] = row["rate"] * row["item_count"]
-        detail_obj["unit"] = row["unit"]
-        if row["unit"] == 'k':
-            total_item_weight += row["item_count"]
-        else:
-            total_item_count += row["item_count"]
-
-        data.append(detail_obj)
-
-    context["bill_detail_list"] = data
-    context["total_item_count"] = total_item_count
-    context["total_item_weight"] = total_item_weight
-
-    return context
 
 @login_required(login_url='/login/')
 @require_http_methods(["GET"])
-def print_bill(request, bill_id):
+def print_bill(request, client_id):
     try:
-        bill_obj = ClientBill.objects.get(id=int(bill_id), is_draft=False)
+        client_obj = Client.objects.get(id=int(client_id))
     except:
         raise Http404
 
-    context = get_print_bill_data(request, bill_obj)
+    if 'bill_date' in request.GET:
+        date = request.GET['bill_date']
+    else:
+        date = ''
+
+    context = get_bill_data(request, client_obj, date)
     return render(request, 'client_bills/print.html', context)
 
 @login_required(login_url='/login/')
